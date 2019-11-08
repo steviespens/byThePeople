ec1872076654316188823558dfe906136fbb1c66
Stevie <stevie@Stevies-MBP.cable.rcn.com>
2019-11-07 21:06:54 -0500

    added regex to register route

--- project/api/urls.py
+++ project/api/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('api/get_user_metadata/', views.get_user_metadata),
     path('api/save_voting_district/', views.save_voting_district),
 
+    url(r'^(/|docket/|about/|polls/|representatives/)?$', include(router.urls)),
     url(r'^(register/|docket/|about/|polls/|representatives/)?api/token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     url(r'^(register/|docket/|about/|polls/|representatives/)?api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
 
